e56a170f1d2305c2c82ad1fb3593e6e75407188d
Nitchamon <Aonoony@gmail.com>
2022-01-18 20:55:50 +0700

    "lab17_1.cpp completed"

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,41 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a, int n, int m){
+	cout << fixed << setprecision(2);
+	for(int i = 0; i < n*m; i++){
+		cout << a[i];
+		if((i+1)%m == 0) cout << endl;
+		else cout << " ";
+	}
+}
+
+void randData(double *a, int n, int m){
+	for(int i = 0; i < n*m; i++){
+		a[i] = (rand()%101)/100.00;
+	}
+}
+
+void findRowSum(const double *a, double *b, int n, int m){
+	int i = 0;
+	for(int j = 0; j < m; j++) b[j] = 0;
+	if(m == 1){
+		for(int k = 0; k < n*m; k++) b[i] = a[i];
+	}else{
+		for(int l = 0; l < n*m; l++){
+			b[i] += a[l];
+			if((l+1)%m == 0) i++;
+		}
+	}
+}
+
+void findColSum(const double*a, double *b, int n, int m){
+	if(n == 1){
+		for(int i = 0; i<m; i++) b[i] = a[i];
+	}else{
+		for(int j=0; j < n*m; j++) b[j%m] += a[j];
+		for(int k = 0; k < m; k++) b[k] = 0;
+		for(int l = 0; l < n*m; l++) b[l%m] += a[l];
+	}
+}
